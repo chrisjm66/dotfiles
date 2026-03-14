94d108ad920ac7ebc23d1a827dd9a7a1cdee3bef
Chris Mangan <chrisjm66@gmail.com>
2026-03-14 11:08:23 -0400

    pls work nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,5 +4,11 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.opt.guifont="JetBrains Mono NL:h12"
-require("config.lazy")
 
+vim.filetype.add({
+	pattern = {
+		[".*%.blade%.php"] = "blade",
+	},
+})
+
+require("config.lazy")
